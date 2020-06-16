fb5cb78fff1be130ee85a4afc17c256a0e07e59c
jcs-lambda <57103874+jcs-lambda@users.noreply.github.com>
2020-06-16 17:06:35 -0500

    refactor agnostic binary search

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -20,13 +20,33 @@ def binary_search(arr, target, start, end):
 # You can implement this function either recursively 
 # or iteratively
 def agnostic_binary_search(arr, target):
-    # descending order
-    if arr[0] > arr[-1]:
-        # binary_search of reversed arr
-        result = binary_search(arr[::-1], target, 0, len(arr) - 1)
-        # found target, adjust result
-        if result != -1:
-            result = abs(len(arr) - 1 - result)
-        return result
-    # ascending order
-    return binary_search(arr, target, 0, len(arr) - 1)
+    # markers for each end of range that can contain the target
+    lowest_index, highest_index = 0, len(arr) - 1
+    # index of target in arr (-1 is not found)
+    found = -1
+
+    # check for ascending / descending, grab appropriate comparison operator
+    if arr[0] < arr[1]: # ascending
+        compare = target.__le__
+    else: # descending
+        compare = target.__gt__
+
+    # loop until target found or list exhausted
+    while lowest_index <= highest_index and found < 0:
+        # marker for center of range that can contain target
+        center_index = (lowest_index + highest_index) // 2
+
+        # current center equals target, update found index
+        if arr[center_index] == target:
+            found = center_index
+        # use apprpriate comparison based on ascending or descending order,
+        # if True, target is to the left of center_index,
+        # adjust highest_index to one less than current center_index
+        elif compare(arr[center_index]):
+            highest_index = center_index - 1
+        # target is to the right of center_index,
+        # adjust lowest_index to one more than current center_index
+        else:
+            lowest_index = center_index + 1
+
+    return found
